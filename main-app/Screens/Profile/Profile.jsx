4ba70717ba9737fa281f78e55bae08f5e2327eea
Daniel Bernal <78066378+HIPERMAST@users.noreply.github.com>
2022-11-13 16:16:27 -0500

    Changes_Profile

---
+++ main-app/Screens/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import LogoComponent from 'main-app/SharedComponents/LogoComponent.jsx';
+
+
+const Profile = () => {
+    return(
+        <View style = {styles.container}>
+            <LogoComponent style = {styles.logo}/>
+        </View>
+
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    
+    },
+    logo: {
+        resizeMode: 'contain',
+        width: 300,
+        height: 300,
+    }
+
+
+})
+
+
+export default Profile;
