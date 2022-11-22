348fedc091af485e04fab278d976d0112b5e6868
Daniel Bernal <78066378+HIPERMAST@users.noreply.github.com>
2022-11-22 09:51:10 -0500

    Sirvió el get pero no muestra nada

--- main-app/Screens/service.js
+++ main-app/Screens/service.js
@@ -1,13 +1,29 @@
+// import env from './.env.json';
 
-export const call = async (method, endpoint, body, token) => {
-    return await fetch(`${env.BACKEND_URL}/${endpoint}`, {
-        method: `${method}`,
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(body) 
-    }).then(response => response.json())
-    .catch((error) => console.error('Error:', error));
-};
+
+// export const call = async (method, endpoint, body, token) => {
+//     return await fetch(`${env.BACKEND_URL}/${endpoint}`, {
+//         method: `${method}`,
+//         mode: 'cors',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${token}`
+//         },
+//         body: ''//JSON.stringify({})
+//     }).then(response => response.json())
+//     .catch((error) => console.error('Error:', error));
+// };
+
+
+// export const callGet = async (method, endpoint, body, token) => {
+//     return await fetch(`${env.BACKEND_URL}/${endpoint}`, {
+//         method: `${method}`,
+//         mode: 'cors',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${token}`
+//         },
+//         body: ''//JSON.stringify()
+//     }).then(response => response.json())
+//     .catch((error) => console.error('Error:', error));
+// };
